3014e27669cf9f5e3a6bc8b7627a17100b90d9da
Timo Westland <timo.westland@incentro.com>
2017-08-16 22:36:05 +0200

    Fixed problem with auth0_domain config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  AUTH0_DOMAIN: 'https://timowestland.eu.auth0.com/',
+  AUTH0_DOMAIN: 'timowestland.eu.auth0.com',
   AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
   MONGO_URI: process.env.MONGO_URI || 'mongodb://admin:admin@ds131583.mlab.com:31583/mean-rsvp',
   NAMESPACE: 'http://myapp.com/roles'
